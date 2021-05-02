70a6670d9c7e18d746e0b69502cebc8bea8d3db2
SBXSYZL <2245440426@qq.com>
2021-05-03 01:01:52 +0800

    表单提交方法bug修复

---
+++ app/src/main/java/com/yiflyplan/app/core/http/FormField.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2021 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.yiflyplan.app.core.http;
+
+import android.os.Build;
+
+import java.util.Objects;
+
+import androidx.annotation.RequiresApi;
+
+public class FormField<T> {
+    private String fieldName;
+    private T fieldValue;
+    private String contentType;
+    private Pair[] extras;
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public T getFieldValue() {
+        return fieldValue;
+    }
+
+    public String getContentType() {
+        return contentType == null ? "" : contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public void setFieldValue(T fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+
+    public Pair[] getExtras() {
+        return extras;
+    }
+
+    public void setExtras(Pair... extras) {
+        this.extras = extras;
+    }
+
+    public static class Pair<K, V> {
+        private K key;
+        private V value;
+
+        public Pair() {
+        }
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair<?, ?> pair = (Pair<?, ?>) o;
+            return key.equals(pair.key) &&
+                    value.equals(pair.value);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+    }
+}
