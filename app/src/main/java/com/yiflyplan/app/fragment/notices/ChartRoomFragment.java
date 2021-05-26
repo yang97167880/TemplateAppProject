6879da5713a95ac99b86b0ad3e48d87f884927a9
SBXSYZL <2245440426@qq.com>
2021-05-26 22:36:13 +0800

    websocket连接完成

--- app/src/main/java/com/yiflyplan/app/fragment/notices/ChartRoomFragment.java
+++ app/src/main/java/com/yiflyplan/app/fragment/notices/ChartRoomFragment.java
@@ -43,6 +43,7 @@ import com.xuexiang.xpage.utils.TitleUtils;
 import com.xuexiang.xui.widget.imageview.RadiusImageView;
 import com.xuexiang.xui.widget.imageview.strategy.impl.GlideImageLoadStrategy;
 import com.xuexiang.xui.widget.toast.XToast;
+import com.xuexiang.xutil.tip.ToastUtils;
 import com.yiflyplan.app.R;
 import com.yiflyplan.app.adapter.VO.CurrentUserVO;
 import com.yiflyplan.app.adapter.VO.OrganizationVO;
@@ -55,12 +56,16 @@ import com.yiflyplan.app.utils.DemoDataProvider;
 import com.yiflyplan.app.utils.MapDataCache;
 import com.yiflyplan.app.utils.XToastUtils;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -71,7 +76,7 @@ import java.util.concurrent.Executors;
 import butterknife.BindView;
 import me.samlss.broccoli.Broccoli;
 
-@Page(name="成员A")
+@Page(name = "成员A")
 public class ChartRoomFragment extends BaseFragment {
 
     @BindView(R.id.chatRoom_recyclerView)
@@ -86,34 +91,35 @@ public class ChartRoomFragment extends BaseFragment {
 
     // 主线程Handler
     // 用于将从服务器获取的消息显示出来
-    private Handler mMainHandler;
+//    private Handler mMainHandler;
 
     // Socket变量
-    private Socket socket;
+//    private Socket socket;
+//
+    private WebSocketClient chatSocket;
 
     // 线程池
     // 为了方便展示,此处直接采用线程池进行线程管理,而没有一个个开线程
-    private ExecutorService mThreadPool;
+//    private ExecutorService mThreadPool;
 
     /**
      * 接收服务器消息 变量
      */
     // 输入流对象
-    InputStream is ;
+//    InputStream is;
 
     // 输入流读取器对象
-    InputStreamReader isr ;
-    BufferedReader br ;
+//    InputStreamReader isr;
+//    BufferedReader br;
 
     // 接收服务器发送过来的消息
-    String response;
+//    String response;
 
     /**
      * 发送消息到服务器 变量
      */
     // 输出流对象
-    OutputStream outputStream;
-
+//    OutputStream outputStream;
 
 
     private List<ChartInfo> chartInfos = new ArrayList<>();
@@ -132,13 +138,13 @@ public class ChartRoomFragment extends BaseFragment {
         Bundle bundle = getArguments();
         currentUserVO = (CurrentUserVO) bundle.getSerializable("currentUserVO");
         if (currentUserVO != null) {
-            return TitleUtils.addTitleBarDynamic((ViewGroup) mRootView,currentUserVO.getUserName(), new View.OnClickListener() {
+            return TitleUtils.addTitleBarDynamic((ViewGroup) mRootView, currentUserVO.getUserName(), new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     popToBack();
                 }
             });
-        }else {
+        } else {
             return super.initTitleBar();
         }
     }
@@ -147,38 +153,37 @@ public class ChartRoomFragment extends BaseFragment {
     protected void initViews() {
 
         Bundle bundle = getArguments();
+        assert bundle != null;
         currentUserVO = (CurrentUserVO) bundle.getSerializable("currentUserVO");
 
-        user = (CurrentUserVO)MapDataCache.getCache("user",null);
+        user = (CurrentUserVO) MapDataCache.getCache("user", null);
         int userId = user.getUserId();
         int leftUserId = currentUserVO.getUserId();
 
 
         // 初始化线程池
-        mThreadPool = Executors.newCachedThreadPool();
+//        mThreadPool = Executors.newCachedThreadPool();
 
 
 // 实例化主线程,用于更新接收过来的消息
-        mMainHandler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case 0:
-                        chartInfos.add(new ChartInfo(currentUserVO.getUserAvatar(),response,0));
-                        mChartAdapter.refresh(chartInfos);
-                        mChartAdapter.setSelectPosition(chartInfos.size()-1);
-                        recyclerView.scrollToPosition(mChartAdapter.getSelectPosition());
-                        break;
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + msg.what);
-                }
-            }
-        };
+//        mMainHandler = new Handler() {
+//            @Override
+//            public void handleMessage(Message msg) {
+//                switch (msg.what) {
+//                    case 0:
+//                        chartInfos.add(new ChartInfo(currentUserVO.getUserAvatar(), response, 0));
+//                        mChartAdapter.refresh(chartInfos);
+//                        mChartAdapter.setSelectPosition(chartInfos.size() - 1);
+//                        recyclerView.scrollToPosition(mChartAdapter.getSelectPosition());
+//                        break;
+//                    default:
+//                        throw new IllegalStateException("Unexpected value: " + msg.what);
+//                }
+//            }
+//        };
 
-        connectSocket(userId,leftUserId);
+        connectSocket(userId, leftUserId);
 //        getMessage();
-
-
 
 
         VirtualLayoutManager virtualLayoutManager = new VirtualLayoutManager(Objects.requireNonNull(getContext()));
@@ -190,48 +195,47 @@ public class ChartRoomFragment extends BaseFragment {
         mChartAdapter = new BroccoliSimpleDelegateAdapter<ChartInfo>(R.layout.adapter_chart_item, new LinearLayoutHelper(), DemoDataProvider.getEmptyInfo(ChartInfo.class)) {
             @Override
             protected void onBindData(MyRecyclerViewHolder holder, ChartInfo model, int position) {
-                if(model != null){
+                if (model != null) {
 //                    int position  = model.getPosition()==0?View.GONE:View.VISIBLE;
 //                    holder.bindDataToViewById(view ->{
 //
 //                    },R.id.right_chart);
                     int avatar_popup_id;
                     int avatar_text_id;
-                    if(model.getPosition() == 0){
+                    if (model.getPosition() == 0) {
                         avatar_popup_id = R.id.avatar_popup_left;
                         avatar_text_id = R.id.avatar_text_left;
                         holder.bindDataToViewById(view -> {
                             RelativeLayout layout = (RelativeLayout) view;
                             layout.setVisibility(View.GONE);
-                        },R.id.right_chart);
+                        }, R.id.right_chart);
                         holder.bindDataToViewById(view -> {
                             LinearLayout layout = (LinearLayout) view;
                             layout.setVisibility(View.VISIBLE);
-                        },R.id.left_chart);
-                    }else{
+                        }, R.id.left_chart);
+                    } else {
                         avatar_popup_id = R.id.avatar_popup_right;
                         avatar_text_id = R.id.avatar_text_right;
                         holder.bindDataToViewById(view -> {
                             RelativeLayout layout = (RelativeLayout) view;
                             layout.setVisibility(View.VISIBLE);
-                        },R.id.right_chart);
+                        }, R.id.right_chart);
                         holder.bindDataToViewById(view -> {
                             LinearLayout layout = (LinearLayout) view;
                             layout.setVisibility(View.GONE);
-                        },R.id.left_chart);
+                        }, R.id.left_chart);
                     }
                     holder.bindDataToViewById(view -> {
                         RadiusImageView avatar = (RadiusImageView) view;
                         GlideImageLoadStrategy img = new GlideImageLoadStrategy();
                         img.loadImage(avatar, Uri.parse(model.getAvatar()));
-                    },avatar_popup_id);
+                    }, avatar_popup_id);
                     holder.bindDataToViewById(view -> {
                         TextView textview = (TextView) view;
                         textview.setText(model.getContent());
-                    },avatar_text_id);
+                    }, avatar_text_id);
                 }
             }
-
 
 
             @Override
@@ -246,6 +250,7 @@ public class ChartRoomFragment extends BaseFragment {
         recyclerView.setAdapter(delegateAdapter);
 
     }
+
     @Override
     protected void initListeners() {
         //下拉刷新
@@ -277,37 +282,37 @@ public class ChartRoomFragment extends BaseFragment {
             @Override
             public void onClick(View v) {
                 String message = edtMessage.getText().toString();
-                if(message.length()==0){
+                if (message.length() == 0) {
                     XToastUtils.error("发送内容不能为空");
-                }else {
+                } else {
 
-                    // 利用线程池直接开启一个线程 & 执行该线程
-                    mThreadPool.execute(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                // 步骤1：从Socket 获得输出流对象OutputStream
-                                // 该对象作用：发送数据
-                                outputStream = socket.getOutputStream();
+//                    // 利用线程池直接开启一个线程 & 执行该线程
+//                    mThreadPool.execute(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            try {
+//                                // 步骤1：从Socket 获得输出流对象OutputStream
+//                                // 该对象作用：发送数据
+//                                outputStream = socket.getOutputStream();
+//
+//                                // 步骤2：写入需要发送的数据到输出流对象中
+//                                outputStream.write((message + "\n").getBytes("utf-8"));
+//                                // 特别注意：数据的结尾加上换行符才可让服务器端的readline()停止阻塞
+//
+//                                // 步骤3：发送数据到服务端
+//                                outputStream.flush();
+//
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//
+//                        }
+//                    });
 
-                                // 步骤2：写入需要发送的数据到输出流对象中
-                                outputStream.write((message+"\n").getBytes("utf-8"));
-                                // 特别注意：数据的结尾加上换行符才可让服务器端的readline()停止阻塞
 
-                                // 步骤3：发送数据到服务端
-                                outputStream.flush();
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-
-                        }
-                    });
-
-
-                    chartInfos.add(new ChartInfo(user.getUserAvatar(),message,1));
+                    chartInfos.add(new ChartInfo(user.getUserAvatar(), message, 1));
                     mChartAdapter.refresh(chartInfos);
-                    mChartAdapter.setSelectPosition(chartInfos.size()-1);
+                    mChartAdapter.setSelectPosition(chartInfos.size() - 1);
                     recyclerView.scrollToPosition(mChartAdapter.getSelectPosition());
                     edtMessage.setText("");
                 }
@@ -319,65 +324,99 @@ public class ChartRoomFragment extends BaseFragment {
 
     /**
      * 连接Socket
+     *
      * @param fromUserId
      * @param toUserId
      */
-    private void connectSocket(int fromUserId,int toUserId){
-        // 利用线程池直接开启一个线程 & 执行该线程
-        mThreadPool.execute(new Runnable() {
+    private void connectSocket(int fromUserId, int toUserId) {
+        ToastUtils.toast("--------------------------建立连接开始--------------------------------");
+        System.out.printf("from %s to %s\n", fromUserId, toUserId);
+        WebSocketClient chatSocket = new WebSocketClient(URI.create(String.format("ws://118.190.97.125:8080/ws/chat/simple/%s/%s", fromUserId, toUserId))) {
             @Override
-            public void run() {
-
-                try {
-
-                    // 创建Socket对象 & 指定服务端的IP 及 端口号
-                    socket  = new Socket("118.190.97.125",8080);
-
-                    // 判断客户端和服务器是否连接成功
-                    System.out.println(socket.isConnected());
-                    Log.d("======", String.valueOf(socket.isConnected()));
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+            public void onOpen(ServerHandshake handshakedata) {
+                ToastUtils.toast(String.format("from %s to %s建立成功", fromUserId, toUserId));
+                Log.d("WEBSOCKET", "connected");
             }
-        });
+
+            @Override
+            public void onMessage(String message) {
+                Log.d("WEBSOCKET", message);
+                ToastUtils.toast(message);
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+                Log.d("WEBSOCKET", "closed");
+            }
+
+            @Override
+            public void onError(Exception ex) {
+                ToastUtils.toast("建立失败");
+                Log.d("WEBSOCKET", "error");
+            }
+        };
+        try {
+            chatSocket.connectBlocking();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        // 利用线程池直接开启一个线程 & 执行该线程
+//        mThreadPool.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//
+////                try {
+////
+////                    // 创建Socket对象 & 指定服务端的IP 及 端口号
+////                    socket  = new Socket("118.190.97.125",8080);
+////
+////                    // 判断客户端和服务器是否连接成功
+////                    System.out.println(socket.isConnected());
+////                    Log.d("======", String.valueOf(socket.isConnected()));
+////
+////
+////                } catch (IOException e) {
+////                    e.printStackTrace();
+////                }
+//
+//
+//
+//            }
+//        });
     }
 
-    private void getMessage(){
-        // 利用线程池直接开启一个线程 & 执行该线程
-        mThreadPool.execute(new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    // 步骤1：创建输入流对象InputStream
-                    is = socket.getInputStream();
-
-                    if(is!=null){
-                        // 步骤2：创建输入流读取器对象 并传入输入流对象
-                        // 该对象作用：获取服务器返回的数据
-                        isr = new InputStreamReader(is);
-                        br = new BufferedReader(isr);
-
-                        // 步骤3：通过输入流读取器对象 接收服务器发送过来的数据
-                        response = br.readLine();
-
-                        // 步骤4:通知主线程,将接收的消息显示到界面
-                        Message msg = Message.obtain();
-                        msg.what = 0;
-                        mMainHandler.sendMessage(msg);
-                    }
-
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
+    private void getMessage() {
+//        // 利用线程池直接开启一个线程 & 执行该线程
+//        mThreadPool.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                try {
+//                    // 步骤1：创建输入流对象InputStream
+//                    is = socket.getInputStream();
+//
+//                    if (is != null) {
+//                        // 步骤2：创建输入流读取器对象 并传入输入流对象
+//                        // 该对象作用：获取服务器返回的数据
+//                        isr = new InputStreamReader(is);
+//                        br = new BufferedReader(isr);
+//
+//                        // 步骤3：通过输入流读取器对象 接收服务器发送过来的数据
+//                        response = br.readLine();
+//
+//                        // 步骤4:通知主线程,将接收的消息显示到界面
+//                        Message msg = Message.obtain();
+//                        msg.what = 0;
+//                        mMainHandler.sendMessage(msg);
+//                    }
+//
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        });
     }
 
 
