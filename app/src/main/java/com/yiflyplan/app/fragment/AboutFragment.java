8d2479b12927424dc16142c0125386f1e4d459af
yang97167880 <836237106@qq.com>
2021-06-07 02:02:57 +0800

    更新下载2

--- app/src/main/java/com/yiflyplan/app/fragment/AboutFragment.java
+++ app/src/main/java/com/yiflyplan/app/fragment/AboutFragment.java
@@ -17,7 +17,14 @@
 
 package com.yiflyplan.app.fragment;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -30,6 +37,7 @@ import com.xuexiang.xui.widget.grouplist.XUIGroupListView;
 import com.xuexiang.xutil.app.AppUtils;
 import com.yiflyplan.app.utils.TokenUtils;
 import com.yiflyplan.app.utils.VersionUtils;
+import com.yiflyplan.app.utils.XToastUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -65,6 +73,9 @@ public class AboutFragment extends BaseFragment {
     @BindView(R.id.tv_copyright)
     TextView mCopyrightTextView;
 
+    String newVersion;
+    String appUrl;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_about;
@@ -75,6 +86,33 @@ public class AboutFragment extends BaseFragment {
 
         mVersionTextView.setText(String.format("版本号：%s", VersionUtils.Version));
 
+        MyHttp.getDownload("/system/getNewestAppDownloadUrl", TokenUtils.getToken(), new MyHttp.Callback() {
+            @Override
+            public void success(JSONObject data) throws JSONException {
+                appUrl = data.getString("data");
+            }
+
+            @Override
+            public void fail(JSONObject error) throws JSONException {
+
+            }
+        });
+        MyHttp.getDownload("/system/getNewestAppVersion", TokenUtils.getToken(), new MyHttp.Callback() {
+            @Override
+            public void success(JSONObject data) throws JSONException {
+                newVersion = data.getString("data");
+            }
+
+            @Override
+            public void fail(JSONObject error) throws JSONException {
+
+            }
+        });
+        try{
+            Thread.sleep(1000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         XUIGroupListView.newSection(getContext())
                 .addItemView(mAboutGroupListView.createItemView(getResources().getString(R.string.about_item_homepage)), v -> AgentWebActivity.goWeb(getContext(), getString(R.string.url_project)))
                 .addTo(mAboutGroupListView);
@@ -82,7 +120,13 @@ public class AboutFragment extends BaseFragment {
                 .addItemView(mAboutGroupListView.createItemView(getResources().getString(R.string.about_item_update)), v -> {new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        download();
+                        Looper.prepare();
+                        if(download()){
+                            XToastUtils.success("已是最新版本");
+                        }else{
+                            XToastUtils.success("更新成功");
+                        }
+                        Looper.loop();
                     }
                 }).start();})
                 .addTo(mAboutGroupListView);
@@ -93,55 +137,64 @@ public class AboutFragment extends BaseFragment {
     }
 
     private boolean download() {
-        MyHttp.getDownload("/system/getNewestAppDownloadUrl", TokenUtils.getToken(), new MyHttp.Callback() {
-            @Override
-            public void success(JSONObject data) throws JSONException {
-                String appUrl = data.getString("data");
-                try {
-                    URL url = new URL(appUrl);
-                    //打开连接
-                    URLConnection conn = url.openConnection();
-                    //打开输入流
-                    InputStream is = conn.getInputStream();
-                    //获得长度
-                    int contentLength = conn.getContentLength();
-                    Log.e("", "文件长度 = " + contentLength);
-                    //创建文件夹 MyDownLoad，在存储卡下
-                    String dirName = Environment.getExternalStorageDirectory() + "/";
-                    //下载后的文件名
-                    String fileName = dirName + "001";
-                    File file1 = new File(fileName);
+        try {
 
-                    if (file1.exists()) {
-                        Log.e("文件-----", "文件已经存在！");
-                    } else {
-                        //创建字节流
-                        byte[] bs = new byte[1024];
-                        int len;
-                        OutputStream os = new FileOutputStream(fileName);
-                        //写数据
-                        while ((len = is.read(bs)) != -1) {
-                            os.write(bs, 0, len);
-                        }
-                        //完成后关闭流
-                        Log.e("文件不存在", "下载成功！");
-                        os.close();
-                        is.close();
-                    }
+            URL url = new URL(appUrl);
+            //打开连接
+            URLConnection conn = url.openConnection();
+            //打开输入流
+            InputStream is = conn.getInputStream();
+            //获得长度
+            int contentLength = conn.getContentLength();
+            Log.e("", "文件长度 = " + contentLength);
+            //创建文件夹 MyDownLoad，在存储卡下
+            String dirName = Environment.getExternalStorageDirectory() + "/";
+            //下载后的文件名
+            String fileName = dirName + "001";
+            File file1 = new File(fileName);
+            if (file1.exists()) {
 
-                } catch (Exception e) {
-                    e.printStackTrace();
+                Log.e("文件-----", "文件已经存在！");
+                return fileIsExists(fileName);
+            } else {
+                //创建字节流
+                byte[] bs = new byte[1024];
+                int len;
+                if(!isGrantExternalRW(this.getActivity())){
+                    return false;
                 }
+                file1.createNewFile();
+                OutputStream os = new FileOutputStream(fileName);
+                //写数据
+                while ((len = is.read(bs)) != -1) {
+                    os.write(bs, 0, len);
+                }
+                //完成后关闭流
+                Log.e("文件不存在", "下载成功！");
+                os.close();
+                is.close();
 
             }
 
-            @Override
-            public void fail(JSONObject error) throws JSONException {
-
-            }
-        });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return false;
+    }
+
+    //动态申请权限
+    public static boolean isGrantExternalRW(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            activity.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+            return false;
+        }
+        return true;
     }
 
     //判断文件是否存在
@@ -158,4 +211,5 @@ public class AboutFragment extends BaseFragment {
 
         return true;
     }
+
 }
